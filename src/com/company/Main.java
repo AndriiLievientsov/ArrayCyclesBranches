a00eab2bf328eb1091e6deba7419a5bd1bfc2928
Apafios <andreiilevencov@gmail.com>
2022-05-09 20:31:35 +0300

    дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,7 +19,8 @@ public class Main {
         array4 [0] = 5.12;
         array4 [1] = 2.1;
         array4 [2] = 10.97;
-        System.out.println("Double array: " + Arrays.toString(array4) + "\n");
+        System.out.println("Double array: " + "\n"
+                + Arrays.toString(array4) + "\n");
 
     //5.   Создайте пустой массив на 10 элементов. Заполните значения элементов. Расширьте массив на 5 элементов.
         int[] array5 = new int[]{77,34,9,214,99,100,5,37,4,1};
@@ -50,12 +51,24 @@ public class Main {
                 maxIndex = i;
             }
         }
-        System.out.println("\n" + "Maximum element index: " + maxIndex);
+        System.out.println("\n" + "Maximum element index: " + maxIndex + "\n");
 
     /*7.   Создайте массив из 8 случайных целых чисел из отрезка `[1;10]`.
     Выведите массив на экран в строку. Замените каждый элемент с нечётным индексом на ноль.
     Снова выведете массив на экран на отдельной строке.*/
-        
+        int[] array8 = new int[8];
+        for (int i = 0; i <array8.length ; i++) {
+            array8[i] = ((int)(Math.random() * 10) + 1);
+        }
+        System.out.println("Primary array:");
+        System.out.println(Arrays.toString(array8));
+        for (int i = 0; i <array8.length ; i++) {
+            if (i % 2 == 1) {
+                array8[i] = 0;
+            }
+        }
+        System.out.println("Modified array:");
+        System.out.println(Arrays.toString(array8)+"\n");
 
     }
 }
